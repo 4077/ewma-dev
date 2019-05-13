0501afea4e8d7e587dbaa4dfd711206c9ff508b0
fed <platinumaccount@yandex.ru>
2019-05-13 15:44:34 +0300

    modules real path (vendor paths tmp problem)+ (node instance select)

--- ui/node/-/controllers/main/content/data/xhr.php
+++ ui/node/-/controllers/main/content/data/xhr.php
@@ -8,12 +8,21 @@ class Xhr extends \Controller
     {
         $this->smap('~|', 'module_path, node_path, type');
 
-        $instance = $this->data['module_path'] . '/_/' . $this->data['node_path'];
+        $instance = $this->getModuleRealPath($this->data['module_path']) . '/_/' . $this->data['node_path'];
 
         $s = &$this->s('<|' . $instance);
 
         $s['node_instance'] = $this->data['node_instance'];
 
         $this->c('~:reload|');
+    }
+
+    public function getModuleRealPath($modulePath)
+    {
+        $modulesTree = \ewma\dev\Svc::getInstance()->getModulesTree();
+
+        $moduleCache = ap($modulesTree, $modulePath);
+
+        return ap($moduleCache, '-/settings/path');
     }
 }
